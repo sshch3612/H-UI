c73c57d61a76b4969d15e67453b5147a066264d2
sshch3612 <1758974924@qq.com>
2019-04-29 22:03:27 +0800

    修改flexitem的less文件

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,7 @@ import Wingblank from "./WingBlank/Wingblank";
 import List  from './List/List';
 import ListItem from './List/ListItem';
 import Flex from  './Grid/Flex';
+import FlexItem from './Grid/FlexItem';
 
 
-export { Whitespace, Wingblank , Flex, List, ListItem};
+export { Whitespace, Wingblank , Flex, FlexItem, List, ListItem};
