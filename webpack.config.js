11ab62acdf08eb5d308671c6ead14c184a63e3c4
sshch3612 <1758974924@qq.com>
2019-04-29 22:01:27 +0800

    修改配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const baseConfig = require("./config/config");
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+// 文件优化 1 css 抽离 mini-css-extract-plugin  打包 压缩
 module.exports = {
   mode: "development",
   entry: {
@@ -14,13 +17,13 @@ module.exports = {
     publicPath: "/",
     filename: "[name].js"
   },
-  devtool: "source-map",
+  // devtool: "source-map",
   module: {
     rules: [
-      { test: /\.(js|ts|tsx)$/, exclude: /node_modules/,use:[
-        "babel-loader",
-         "ts-loader",
-      ] 
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"]
       },
       // {
       //   test: /\.(less|css)$/,
@@ -33,19 +36,35 @@ module.exports = {
       // },
       {
         test: /\.(less|css)$/,
-        use: ["style-loader", "css-loader", "less-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // 这里可以指定一个 publicPath
+              // 默认使用 webpackOptions.output中的publicPath
+              // publicPath: '../'
+            }
+          },
+          "css-loader",
+          "less-loader"
+        ]
+        //  ["style-loader", "css-loader", "less-loader"],
         // include: baseConfig.cssModulesExcludes || []
       },
       { test: /\.html$/, loader: "html-loader" },
-      {
-        test: /\.(jpg|png|gif)$/,
-        loader: "url-loader",
-        options: { limit: 50000, name: "images/[name]-[hash:8].[ext]" }
-      },
+      // {
+      //   test: /\.(jpg|png|gif)$/,
+      //   loader: "url-loader",
+      //   options: { limit: 50000, name: "images/[name]-[hash:8].[ext]" }
+      // },
       {
         test: /\.(mp3|eot|svg|ttf|woff|woff2|otf)$/,
-        loader: "url-loader",
-        options: { limit: 0, name: "audio/[name]-[hash:8].[ext]" }
+        // loader: "url-loader",
+        // options: { limit: 0, name: "audio/[name]-[hash:8].[ext]" }
+        use:[
+          {loader:'file-loader', options:{}},
+          {loader:'url-loader',options: { limit: 0, name: "audio/[name]-[hash:8].[ext]" }}
+        ]
       }
     ]
   },
@@ -55,7 +74,30 @@ module.exports = {
       template: "./public/index.html",
       chunks: ["index"]
     }),
-    new CleanWebpackPlugin(["dist"])
+    new CleanWebpackPlugin(["dist"]),
+    // new ExtractTextPlugin('css/[name].css'),
+    new MiniCssExtractPlugin({
+      // 类似 webpackOptions.output里面的配置 可以忽略
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      // cssProcessorOptions: cssnanoOptions,
+      cssProcessorPluginOptions: {
+        preset: [
+          "default",
+          {
+            discardComments: {
+              removeAll: true
+            },
+            normalizeUnicode: false
+          }
+        ]
+      },
+      canPrint: true
+    })
   ],
   resolve: {
     alias: {
